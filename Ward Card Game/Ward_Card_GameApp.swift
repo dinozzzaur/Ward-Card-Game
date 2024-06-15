bb5b81da057709896c61317c3462ecdf4ddf854d
dinozzzaur <dinasyl19satynbek@gmail.com>
2024-06-15 20:45:27 +0300

    Initial Commit

---
+++ Ward Card Game/Ward_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ward_Card_GameApp.swift
+//  Ward Card Game
+//
+//  Created by Dınasyl Satynbek on 14.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ward_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ward Card GameUITests/Ward_Card_GameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ward_Card_GameUITestsLaunchTests.swift
+//  Ward Card GameUITests
+//
+//  Created by Dınasyl Satynbek on 14.06.2024.
+//
+
+import XCTest
+
+final class Ward_Card_GameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
